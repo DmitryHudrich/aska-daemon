08ba963a81070db4e8250fc498aece9b30bf9b6c
madeinheaven91 <80648492+madeinheaven91@users.noreply.github.com>
2024-08-01 21:54:59 +0300

    implemented disk fetch

--- src/serverside/service/fetchservice/infobyfilter.rs
+++ src/serverside/service/fetchservice/infobyfilter.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
+
 use multimap::MultiMap;
-use serde_json::{json, Number, Value};
-use sysinfo::System;
+use serde_json::{json, Map, Number, Value};
+use sysinfo::{Disks, System};
 
 pub fn new(params: MultiMap<String, String>) -> serde_json::Value {
-    let mut res = MultiMap::new();
+    let mut res = HashMap::new();
     for (key, value) in &params {
         for element in value {
             let val = match_param(key.as_str(), element.as_str());
@@ -16,7 +18,7 @@ pub fn new(params: MultiMap<String, String>) -> serde_json::Value {
 
 /* статистика обхвата члена */
 fn match_param(key: &str, value: &str) -> serde_json::Value {
-    if value != "1" {
+    if value != "1" && key != "diname" {
         return Value::Null;
     }
     // Prefixes list for field names:
@@ -29,6 +31,7 @@ fn match_param(key: &str, value: &str) -> serde_json::Value {
     //  "siname" means system_info_name
     //  "sikernel_version" means system_info_kernel_version
     let system = System::new_all();
+    let disks = Disks::new_with_refreshed_list();
 
     match key {
         "siname" => Value::String(System::name().unwrap()),
@@ -39,14 +42,35 @@ fn match_param(key: &str, value: &str) -> serde_json::Value {
         "miused" => Value::Number(Number::from(system.used_memory())),
         "miswap_total" => Value::Number(Number::from(system.total_swap())),
         "miswap_used" => Value::Number(Number::from(system.used_swap())),
-        "di_test" => json!({
-        // TODO: хевен сделай пж с дисками суету
-            "/": {
-                "name": "bebra",
-                "desc": "sex",
+        "didisks" => {
+            let mut disk_map: Map<String, Value> = Map::new();
+            for disk in &disks {
+                // FIXME: как будто говнокод. мне кажется,можно сделать лучше
+                let mut disk_info: Map<String, Value> = Map::new();
+                disk_info.insert("name".to_string(), json!(disk.name().to_str()));
+                disk_info.insert("total_space".to_string(), json!(disk.total_space()));
+                disk_info.insert("available_space".to_string(), json!(disk.available_space()));
+                disk_info.insert("kind".to_string(), json!(disk.kind().to_string()));
+                disk_info.insert(
+                    "file_system".to_string(),
+                    json!(disk.file_system().to_str().unwrap().to_string()),
+                );
+                disk_info.insert("is_removable".to_string(), json!(disk.is_removable()));
+                disk_info.insert("used_space".to_string(), json!(disk.total_space() - disk.available_space()));
+                disk_map.insert(
+                    disk.mount_point().to_str().unwrap().to_string(),
+                    Value::Object(disk_info),
+                );
             }
-        }),
-
+            Value::Object(disk_map)
+        },
+        // TODO: инфу о процессорах, интернет подключениях, процессах?? я хз, почему бы и нет
+        // еще вот такой прикол, типа /fetch/?diname="/dev/sda1"&ditotal_space=1
+        // response:
+        // {
+        //  ditotal_space: 23592365
+        // }
+        // чтобы получать инфу конкретно об одном разделе
         _ => Value::Null,
     }
 }
