941c7d52596a7d7f8a2b6a6ba6d49021a996e521
w1thluv <dimahudric@gmail.com>
2024-08-16 19:18:58 +0300

    added tests

--- src/serverside/configuration.rs
+++ src/serverside/configuration.rs
@@ -50,7 +50,7 @@ pub struct Net {
 
 impl Net {
     pub fn port(&self) -> u16 {
-        self.port.unwrap()
+        self.port.unwrap_or_else(||use_default("port", 3000))
     }
 }
 
@@ -65,29 +65,45 @@ pub struct Logging {
 
 impl Logging {
     pub fn place(&self) -> bool {
-        self.place.unwrap()
+        self.place.unwrap_or_else(||use_default("place", false))
     }
 
     pub fn level(&self) -> LevelFilter {
-        self.level.unwrap()
+        self.level
+            .unwrap_or_else(||use_default("level", LevelFilter::Info))
     }
 
-    pub fn folder(&self) -> &String {
-        self.folder.as_ref().expect("bebra")
+    pub fn folder(&self) -> String {
+        self.folder
+            .clone()
+            .unwrap_or_else(||use_default("folder", String::from("logs")))
     }
 
     pub fn filescount(&self) -> usize {
-        self.filescount.unwrap()
+        self.filescount.unwrap_or_else(||use_default("filescount", 10))
     }
 
     pub fn stdout(&self) -> bool {
-        self.stdout.unwrap()
+        self.stdout.unwrap_or_else(||use_default("stdout", true))
     }
+}
+
+fn use_default<T>(field: &str, value: T) -> T
+where
+    T: std::fmt::Debug,
+{
+    warn!(
+        "Field {} not found, using default value: {:?}",
+        field, value
+    );
+    value
 }
 
 #[cfg(test)]
 mod tests {
-    use super::get;
+    use log::LevelFilter;
+
+    use super::{get, Config, Logging, Net};
 
     #[test] // Should not paniced
     fn config_getters() {
@@ -98,5 +114,131 @@ mod tests {
         _ = config.logging().filescount();
         _ = config.logging().stdout();
         _ = config.logging().folder();
+    }
+
+    #[test]
+    fn port() {
+        let config_with_default_port = default_config();
+        let config_without_port = {
+            let mut c = default_config();
+            c.net.as_mut().unwrap().port = None;
+            c
+        };
+        let config_wit_custom_port = {
+            let mut c = default_config();
+            c.net.as_mut().unwrap().port = Some(2000);
+            c
+        };
+
+        assert_eq!(config_with_default_port.net().port(), 3000);
+        assert_eq!(config_without_port.net().port(), 3000);   
+        assert_eq!(config_wit_custom_port.net().port(), 2000);
+    }
+
+    #[test]
+    fn place() {
+        let config_with_default_place = default_config();
+        let config_without_place = {
+            let mut c = default_config();
+            c.logging.as_mut().unwrap().place = None;
+            c
+        };
+        let config_wit_custom_place = {
+            let mut c = default_config();
+            c.logging.as_mut().unwrap().place = Some(true);
+            c
+        };
+
+        assert!(!config_with_default_place.logging().place());
+        assert!(!config_without_place.logging().place());
+        assert!(config_wit_custom_place.logging().place());
+    }
+
+    #[test]
+    fn level() {
+        let config_with_default_level = default_config();
+        let config_without_level = {
+            let mut c = default_config();
+            c.logging.as_mut().unwrap().level = None;
+            c
+        };
+        let config_with_warn = {
+            let mut c = default_config();
+            c.logging.as_mut().unwrap().level = Some(LevelFilter::Warn);
+            c
+        };
+
+        assert_eq!(config_with_default_level.logging().level(), LevelFilter::Info);
+        assert_eq!(config_without_level.logging().level(), LevelFilter::Info);
+        assert_eq!(config_with_warn.logging().level(), LevelFilter::Warn);
+    }
+
+    #[test]
+    fn folder() {
+        let config_with_default_folder = default_config();
+        let config_without_folder = {
+            let mut c = default_config();
+            c.logging.as_mut().unwrap().folder = None;
+            c
+        };
+        let config_wit_custom_folder = {
+            let mut c = default_config();
+            c.logging.as_mut().unwrap().folder = Some("aska_logs".to_owned());
+            c
+        };
+        assert_eq!(config_with_default_folder.logging().folder(), "logs");
+        assert_eq!(config_without_folder.logging().folder(), "logs");
+        assert_eq!(config_wit_custom_folder.logging().folder(), "aska_logs");
+    }
+
+    #[test]
+    fn filescount() {
+        let config_with_default_filescount = default_config();
+        let config_without_filescount = {
+            let mut c = default_config();
+            c.logging.as_mut().unwrap().filescount = None;
+            c
+        };
+        let config_wit_custom_filescount = {
+            let mut c = default_config();
+            c.logging.as_mut().unwrap().filescount = Some(15);
+            c
+        };
+
+        assert_eq!(config_with_default_filescount.logging().filescount(), 10);
+        assert_eq!(config_without_filescount.logging().filescount(), 10);
+        assert_eq!(config_wit_custom_filescount.logging().filescount(), 15);
+    }
+
+    #[test]
+    fn stdout() {
+        let config_with_default_stdout = default_config();
+        let config_without_stdout = {
+            let mut c = default_config();
+            c.logging.as_mut().unwrap().stdout = None;
+            c
+        };
+        let config_wit_custom_stdout = {
+            let mut c = default_config();
+            c.logging.as_mut().unwrap().stdout = Some(false);
+            c
+        };
+
+        assert!(config_with_default_stdout.logging().stdout());
+        assert!(config_without_stdout.logging().stdout());
+        assert!(!config_wit_custom_stdout.logging().stdout());
+    }
+
+    fn default_config() -> Config {
+        Config {
+            net: Some(Net { port: Some(3000) }),
+            logging: Some(Logging {
+                place: Some(false),
+                level: Some(log::LevelFilter::Info),
+                folder: Some("logs".to_owned()),
+                filescount: Some(10),
+                stdout: Some(true),
+            }),
+        }
     }
 }
