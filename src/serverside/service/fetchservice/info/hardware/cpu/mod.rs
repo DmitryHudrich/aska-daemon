7c389ca1d39dea404eae64f37e8824e40533fc82
vlinearo <vitalliy2005@gmail.com>
2024-08-20 17:09:49 +0300

    rewrite some code with closures (fetches/mnt)

--- src/serverside/service/fetchservice/info/hardware/cpu/mod.rs
+++ src/serverside/service/fetchservice/info/hardware/cpu/mod.rs
@@ -1,32 +1,31 @@
-use serde_json::json;
+use serde_json::{json, Value};
 use sysinfo::{CpuRefreshKind, RefreshKind, System};
 
-type Json = serde_json::Value;
 
-pub fn get_global_usage(_: String) -> Json {
+pub fn get_global_usage(_: String) -> Value {
     json!(system(|sys| sys.global_cpu_usage()))
 }
 
-pub fn get_brand(_: String) -> Json {
+pub fn get_brand(_: String) -> Value {
     json!(system(|sys| sys.cpus()[0].brand().to_string()))
 }
 
-pub fn get_core_count(_: String) -> Json {
+pub fn get_core_count(_: String) -> Value {
     json!(system(|sys| sys.cpus().len()))
 }
 
-pub fn get_vendor(_: String) -> Json {
+pub fn get_vendor(_: String) -> Value {
     json!(system(|sys| sys.cpus()[0].vendor_id().to_string()))
 }
 
-pub fn get_name(_: String) -> Json {
+pub fn get_name(_: String) -> Value {
     json!(system(|sys| sys.cpus()[0].name().to_string()))
 }
 
 // TODO:
 // я не уверен, что это вообще возвращает. каждый раз, когда я собираю
 // данные о частоте процессора, она всегда разная. как это работает?
-pub fn get_frequency(_: String) -> Json {
+pub fn get_frequency(_: String) -> Value {
     json!(system(|sys| sys.cpus()[0].frequency()))
 }
 
--- src/serverside/service/fetchservice/info/hardware/ram/mod.rs
+++ src/serverside/service/fetchservice/info/hardware/ram/mod.rs
@@ -1,33 +1,32 @@
-use serde_json::json;
+use serde_json::{json, Value};
 use sysinfo::{MemoryRefreshKind, RefreshKind, System};
 
-type Json = serde_json::Value;
 
-pub fn get_total_memory(_: String) -> Json {
+pub fn get_total_memory(_: String) -> Value {
     json!(system(|sys| sys.total_memory()))
 }
 
-pub fn get_used_memory(_: String) -> Json {
+pub fn get_used_memory(_: String) -> Value {
     json!(system(|sys| sys.used_memory()))
 }
 
-pub fn get_free_memory(_: String) -> Json {
+pub fn get_free_memory(_: String) -> Value {
     json!(system(|sys| sys.free_memory()))
 }
 
-pub fn get_available_memory(_: String) -> Json {
+pub fn get_available_memory(_: String) -> Value {
     json!(system(|sys| sys.available_memory()))
 }
 
-pub fn get_total_swap(_: String) -> Json {
+pub fn get_total_swap(_: String) -> Value {
     json!(system(|sys| sys.total_swap()))
 }
 
-pub fn get_used_swap(_: String) -> Json {
+pub fn get_used_swap(_: String) -> Value {
     json!(system(|sys| sys.used_swap()))
 }
 
-pub fn get_free_swap(_: String) -> Json {
+pub fn get_free_swap(_: String) -> Value {
     json!(system(|sys| sys.free_swap()))
 }
 
