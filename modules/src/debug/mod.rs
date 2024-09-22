308c96d8809b6750cb3e537e59c2536b19c95933
w1thluv <dimahudric@gmail.com>
2024-09-22 13:58:56 +0300

    oxxxymiron

--- modules/src/debug/mod.rs
+++ modules/src/debug/mod.rs
@@ -1,10 +1,9 @@
-
 use logic::workers_infrastructure::WorkerRunner;
 use tokio::{sync::MutexGuard, task::JoinHandle};
 
 pub(crate) mod workers;
 
-pub fn init_module(runner: MutexGuard<'_, WorkerRunner>) -> Result<JoinHandle<()>, String> {
+pub fn init_module(runner: MutexGuard<WorkerRunner>) -> Result<JoinHandle<()>, String> {
     let mut runner_clone = runner.clone();
     let handle: JoinHandle<()> = tokio::spawn(async move {
         runner_clone.run_workers().await;
