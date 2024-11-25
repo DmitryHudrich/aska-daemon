735aba185cf0e66b4564a5864da9bd90071f5ac8
w1thluv <dimahudric@gmail.com>
2024-11-25 16:53:02 +0300

    formatting tgbot

--- src/crates/tgbot/src/lib.rs
+++ src/crates/tgbot/src/lib.rs
@@ -1,7 +1,6 @@
 use async_trait::async_trait;
 use features::{
     llm_api,
-    services::commands::music::{self, MediaPlayingStatus},
     workers::Observer,
 };
 use shared::{
@@ -17,6 +16,8 @@ use teloxide::{
 };
 
 pub mod prerun;
+
+mod music_dispatching;
 
 #[derive(BotCommands, Clone, Debug)]
 #[command(
@@ -79,7 +80,7 @@ async fn dispatch(cmd: Command, bot: &Bot, msg: &Message) -> Result<(), teloxide
                 .await?;
         }
         Command::Music(command) => {
-            let response = dispatch_music_command(command, msg).await;
+            let response = music_dispatching::dispatch_music_command(command, msg).await;
             bot.send_message(msg.chat.id, response).await?;
         }
         Command::Execute(command) => {
@@ -119,57 +120,6 @@ async fn recognize_command_with_llm(msg: String) -> String {
 //     })
 //     .await;
 // }
-
-async fn dispatch_music_command(command: String, msg: &Message) -> String {
-    match command.as_str() {
-        "pause" | "resume" => {
-            let music_status = music::get_status();
-            music::play_pause();
-            match music_status {
-                MediaPlayingStatus::Stopped => "music is not playing".to_string(),
-                MediaPlayingStatus::Paused(_) => {
-                    let prompt = llm_utils::get_prompt("/telegram/music/resume");
-                    let formatted_prompt = prompt.replace("{command}", msg.text().unwrap());
-                    let response = llm_api::send_request(formatted_prompt);
-                    response.await
-                }
-
-                MediaPlayingStatus::Playing(_) => {
-                    let prompt = llm_utils::get_prompt("/telegram/music/pause");
-                    let formatted_prompt = prompt.replace("{command}", msg.text().unwrap());
-                    let response = llm_api::send_request(formatted_prompt);
-                    response.await
-                }
-                MediaPlayingStatus::Unknown => "music is not playing".to_string(),
-            }
-        }
-        "status" => {
-            let music_status = music::get_status();
-            match music_status {
-                MediaPlayingStatus::Stopped => "music is not playing".to_string(),
-                MediaPlayingStatus::Paused(status) => {
-                    let prompt = llm_utils::get_prompt("/telegram/music/status");
-                    let formatted_prompt = prompt
-                        .replace("{status}", format!("{:?}", status).as_str())
-                        .replace("{message}", msg.text().unwrap());
-                    let response = llm_api::send_request(formatted_prompt);
-                    response.await
-                }
-
-                MediaPlayingStatus::Playing(status) => {
-                    let prompt = llm_utils::get_prompt("/telegram/music/status");
-                    let formatted_prompt = prompt
-                        .replace("{status}", format!("{:?}", status).as_str())
-                        .replace("{message}", msg.text().unwrap());
-                    let response = llm_api::send_request(formatted_prompt);
-                    response.await
-                }
-                MediaPlayingStatus::Unknown => "music is not playing".to_string(),
-            }
-        }
-        _ => todo!(),
-    }
-}
 
 pub struct PrintObserver {
     chatid: ChatId,
---
+++ src/crates/tgbot/src/music_dispatching.rs
@@ -0,0 +1,58 @@
+use features::{
+    llm_api,
+    services::commands::music::{self, MediaPlayingStatus},
+};
+use shared::utils::llm_utils;
+use teloxide::types::Message;
+
+pub async fn dispatch_music_command(command: String, msg: &Message) -> String {
+    match command.as_str() {
+        "pause" | "resume" => {
+            let music_status = music::get_status();
+            music::play_pause();
+            match music_status {
+                MediaPlayingStatus::Stopped => "music is not playing".to_string(),
+                MediaPlayingStatus::Paused(_) => {
+                    let prompt = llm_utils::get_prompt("/telegram/music/resume");
+                    let formatted_prompt = prompt.replace("{command}", msg.text().unwrap());
+                    let response = llm_api::send_request(formatted_prompt);
+                    response.await
+                }
+
+                MediaPlayingStatus::Playing(_) => {
+                    let prompt = llm_utils::get_prompt("/telegram/music/pause");
+                    let formatted_prompt = prompt.replace("{command}", msg.text().unwrap());
+                    let response = llm_api::send_request(formatted_prompt);
+                    response.await
+                }
+                MediaPlayingStatus::Unknown => "music is not playing".to_string(),
+            }
+        }
+        "status" => {
+            let music_status = music::get_status();
+            match music_status {
+                MediaPlayingStatus::Stopped => "music is not playing".to_string(),
+                MediaPlayingStatus::Paused(status) => {
+                    let prompt = llm_utils::get_prompt("/telegram/music/status");
+                    let formatted_prompt = prompt
+                        .replace("{status}", format!("{:?}", status).as_str())
+                        .replace("{message}", msg.text().unwrap());
+                    let response = llm_api::send_request(formatted_prompt);
+                    response.await
+                }
+
+                MediaPlayingStatus::Playing(status) => {
+                    let prompt = llm_utils::get_prompt("/telegram/music/status");
+                    let formatted_prompt = prompt
+                        .replace("{status}", format!("{:?}", status).as_str())
+                        .replace("{message}", msg.text().unwrap());
+                    let response = llm_api::send_request(formatted_prompt);
+                    response.await
+                }
+                MediaPlayingStatus::Unknown => "music is not playing".to_string(),
+            }
+        }
+        _ => todo!(),
+    }
+}
+
